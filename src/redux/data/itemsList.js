93519f87cb1dc23cbb43b97a1dd92b5ba154f2b6
Marian <marianlopig@gmail.com>
2022-05-10 17:36:05 +0200

    structure of files and components

---
+++ src/redux/data/itemsList.js
@@ -0,0 +1,34 @@
+const items = [
+  {
+    id: 1,
+    name: "tomato",
+    price: 3,
+  },
+  {
+    id: 2,
+    name: "potato",
+    price: 1,
+  },
+  {
+    id: 3,
+    name: "bread",
+    price: 3,
+  },
+  {
+    id: 4,
+    name: "banana",
+    price: 4,
+  },
+  {
+    id: 5,
+    name: "flowers",
+    price: 2,
+  },
+  {
+    id: 6,
+    name: "milk",
+    price: 5,
+  },
+];
+
+export default items;
